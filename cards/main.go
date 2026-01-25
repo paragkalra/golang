c9352462e865027cb327118f9d14e1249f7fa9ca
Parag Kalra <paragkalra@gmail.com>
2026-01-25 15:44:46 -0800

    Added a function to return string.

--- cards/main.go
+++ cards/main.go
@@ -4,7 +4,11 @@ import "fmt"
 
 func main() {
 	// var card string = "Ace of Spades"
-	card := "Ace of Spades"
-	card = "Five of Diamonds"
+	// card := "Five of Diamonds"
+	card := newCard()
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds..."
 }
