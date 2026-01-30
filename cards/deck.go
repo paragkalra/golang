49a495e037d1781cf5dda9696a6abb0188b727bf
Parag Kalra <paragkalra@gmail.com>
2026-01-30 15:27:57 -0800

    Created and using a function to use deck of cards.

--- cards/deck.go
+++ cards/deck.go
@@ -9,3 +9,16 @@ func (d deck) print() {
 		fmt.Println(i, card)
 	}
 }
+
+func newDeck() deck {
+
+	cards := deck{}
+	suits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	numbers := []string{"Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"}
+	for _, suit := range suits {
+		for _, number := range numbers {
+			cards = append(cards, number+" of "+suit)
+		}
+	}
+	return cards
+}
--- cards/main.go
+++ cards/main.go
@@ -1,16 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
-	// var card string = "Ace of Spades"
-	// card := "Five of Diamonds"
-	card := newCard()
-	fmt.Println(card)
-	cards := deck{"Ace of Diamonds", "Two of Diamonds", newCard()}
+	cards := newDeck()
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
