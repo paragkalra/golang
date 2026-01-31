19b2e0fa1dac7487330f75fa84ab121a493522aa
Parag Kalra <paragkalra@gmail.com>
2026-01-30 16:21:52 -0800

    Added a function to deal the cards.

--- cards/main.go
+++ cards/main.go
@@ -1,6 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
 	cards.print()
+	fmt.Println("###############################################")
+	hand, remainingCards := deal(cards, 5)
+	hand.print()
+	fmt.Println("###############################################")
+	remainingCards.print()
 }
