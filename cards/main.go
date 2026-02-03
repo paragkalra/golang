a7940df1056e15bfb9d1efd3df07b2590e719491
Parag Kalra <paragkalra@gmail.com>
2026-02-03 13:41:33 -0800

    Added a function to read decks from file.

--- cards/main.go
+++ cards/main.go
@@ -5,11 +5,13 @@ import "fmt"
 func main() {
 	cards := newDeck()
 	cards.print()
-	fmt.Println("###############################################")
+	fmt.Println("#################Dealing cards##############################")
 	hand, remainingCards := deal(cards, 5)
 	hand.print()
-	fmt.Println("###############################################")
+	fmt.Println("#################Remaining cards##############################")
 	remainingCards.print()
-	fmt.Println("###############################################")
+	fmt.Println("#################Saving cards to file##############################")
 	cards.saveToFile("mycards")
+	fmt.Println("#################Reading cards from file##############################")
+	newDeckFromFile("mycards").print()
 }
