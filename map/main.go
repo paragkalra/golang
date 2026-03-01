30e150a0380b7bba8ed98a3082979bfd726bbf6d
Parag Kalra <paragkalra@gmail.com>
2026-03-01 06:20:34 -0800

    Declaring and iterating over maps in Go.

--- map/main.go
+++ map/main.go
@@ -9,4 +9,27 @@ func main() {
 		"blue":  "#0000ff",
 	}
 	fmt.Println(colors)
+
+	var colors2 map[string]string
+	fmt.Println(colors2)
+
+	colors3 := make(map[string]string)
+	fmt.Println(colors3)
+
+	colors3["white"] = "#ffffff"
+	fmt.Println(colors3)
+
+	delete(colors3, "white")
+	fmt.Println(colors3)
+
+	value := colors["red"]
+	fmt.Println(value)
+
+	printMap(colors)
+}
+
+func printMap(m map[string]string) {
+	for key, value := range m {
+		fmt.Println("Hex code for", key, "is", value)
+	}
 }
