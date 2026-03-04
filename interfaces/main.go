3aee4d9bdb29e80eefeb0df8c686804f66f15676
Parag Kalra <paragkalra@gmail.com>
2026-03-03 17:28:10 -0800

    Add Added a starter script ot user interaces in Go.

---
+++ interfaces/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type englishBot struct{}
+type spanishBot struct{}
+type bot interface {
+	getGreeting() string
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	return "Hello!"
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola!"
+}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
