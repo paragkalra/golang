b7bc9995a8be22f596bff005efb959b54de6e890
Parag Kalra <paragkalra@gmail.com>
2026-03-18 16:24:53 -0700

    Modified the channel example to continuously check if the site is up in a loop.

--- channels/main.go
+++ channels/main.go
@@ -24,20 +24,18 @@ func main() {
 		// checkLink(link)
 	}
 
-	fmt.Println(<-c)
-	fmt.Println(<-c)
-	fmt.Println(<-c)
-	fmt.Println(<-c)
-	fmt.Println(<-c)
+	for l := range c {
+		go checkLink(l, c)
+	}
 }
 
 func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
-		c <- "Might be down I think"
+		c <- link
 		return
 	}
 	fmt.Println(link, "is up!")
-	c <- "Is up"
+	c <- link
 }
